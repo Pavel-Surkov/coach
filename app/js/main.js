471b6dbd781ab5e7f8cd891801039f2492d3ab63
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-10-22 18:19:23 +0700

    adds menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.header__menu-button');
+
+menuBtn.onclick = () => {
+	if (menu) {
+		menu.classList.toggle('menu_active');
+		document.documentElement.classList.toggle('.is-locked');
+	}
+};
--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,12 @@
 
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.header__menu-button');
+
+menuBtn.onclick = () => {
+	if (menu) {
+		menu.classList.toggle('menu_active');
+		document.documentElement.classList.toggle('.is-locked');
+	}
+};
