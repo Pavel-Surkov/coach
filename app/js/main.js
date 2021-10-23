ef6e140aa13c8969ce7078f526f1ab45f566b7ce
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-10-23 23:53:40 +0700

    adds faq section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Menu
 const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.header__menu-button');
 
@@ -9,3 +10,20 @@ menuBtn.onclick = () => {
 		document.documentElement.classList.toggle('.is-locked');
 	}
 };
+
+// FAQ accordeon
+const accordeon = document.querySelector('.faq-accordeon');
+
+accordeon.addEventListener('click', (evt) => {
+	const target = evt.target;
+	const accordeonBtn = target.closest('.faq-accordeon__title');
+
+	if (accordeonBtn) {
+		const faqText = accordeonBtn.nextElementSibling;
+
+		if (faqText) {
+			accordeonBtn.classList.toggle('faq-accordeon__title_active');
+			faqText.classList.toggle('faq-accordeon__text_opened');
+		}
+	}
+});
